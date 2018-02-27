a433456b05ab6c2a0768606d688ffac6cd48be3d
Justin Zimmer <jtzimmer6@gmail.com>
2018-02-26 18:32:05 -0700

    initial commit

---
+++ lib/ostip_pbx.ex
@@ -0,0 +1,18 @@
+defmodule OstipPbx do
+  @moduledoc """
+  Documentation for OstipPbx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OstipPbx.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ostip_pbx_test.exs
@@ -0,0 +1,8 @@
+defmodule OstipPbxTest do
+  use ExUnit.Case
+  doctest OstipPbx
+
+  test "greets the world" do
+    assert OstipPbx.hello() == :world
+  end
+end

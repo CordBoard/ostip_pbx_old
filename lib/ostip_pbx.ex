58e5fb62da89277ba9e6fef04fb9ccaa8789567c
Justin Zimmer <jtzimmer6@gmail.com>
2018-02-26 18:54:46 -0700

    converted to phoenix app

--- lib/ostip_pbx.ex
+++ lib/ostip_pbx.ex
@@ -1,18 +1,9 @@
 defmodule OstipPbx do
   @moduledoc """
-  Documentation for OstipPbx.
+  OstipPbx keeps the contexts that define your domain
+  and business logic.
+
+  Contexts are also responsible for managing your data, regardless
+  if it comes from the database, an external API or others.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> OstipPbx.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end

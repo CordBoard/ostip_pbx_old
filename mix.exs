f51f667439337be0dd4b86a5411a1456fac38ead
Justin Zimmer <jtzimmer6@gmail.com>
2018-02-26 18:34:27 -0700

    added exari dependency

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule OstipPbx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ostip_exari, path: "./ostip_exari/swagger/client"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
